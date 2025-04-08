948efb5eaea8413dbe3eb18b6f60f219a26a7587
lucashomero <lucashomerovs@gmail.com>
2025-04-07 23:30:51 -0300

    var

---
+++ mod01/var/sintaxe.py
@@ -0,0 +1,18 @@
+# Sintaxe 
+"""
+
+"""
+
+nome = "Lucas"
+sobrenome = "Homero"
+
+idade = 21
+if idade > 18:
+    print("maior de idade")
+else:   
+    print("menor de idade")
+
+print("nome: %s" % nome)
+print("nome: %s %s" % (nome, sobrenome))
+print("nome: {} {}".format(nome,sobrenome))
+print(f"nome: {nome} {sobrenome}")
